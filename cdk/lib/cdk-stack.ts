26e887a8a0c89b86f448c71f69b489e55980d35c
Rishi Tiwari <rishi008.tiwari@gmail.com>
2026-06-06 13:07:14 +0530

    dynamo s3 and cognito resource created

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,61 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import * as dynamo from 'aws-cdk-lib/aws-dynamodb'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Table, AttributeType, BillingMode, StreamViewType } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
+import { PublicKey, KeyGroup } from 'aws-cdk-lib/aws-cloudfront';
+import { readFileSync } from 'fs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const dynamoDbTable = new dynamo.Table(this, 'MyTable', {
-      partitionKey: { name: 'id', type: dynamo.AttributeType.STRING },
+    // DB For Messages, Groups and Conection data
+    const dynamoDbTable = new Table(this, 'ChatlingsTable', {
       tableName: 'chatlings',
+      partitionKey: { name: 'PK', type: AttributeType.STRING },
+      sortKey: { name: 'SK', type: AttributeType.STRING },
+      stream: StreamViewType.NEW_IMAGE,
+      billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    // Media Files S3 bucket
+    const mediaBucket = new Bucket(this, 'ChatlingsMediaBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      cors: [
+        {
+          allowedMethods: [HttpMethods.PUT],
+          allowedOrigins: ['*'],         // tighten to CloudFront domain after deploy
+          allowedHeaders: ['*'],
+        }
+      ]
+    });
+
+    // Key for Cloudfront signed cookies
+    const publicKey = new PublicKey(this, 'ChatlingsPublicKey', {
+      encodedKey: readFileSync('../keys/public_key.pem', 'utf8'),
+    });
+
+    const keyGroup = new KeyGroup(this, 'ChatlingsKeyGroup', {
+      items: [publicKey],
+    });
+
+    // Cognito for user management
+    const userPool = new UserPool(this, 'ChatlingsUserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const userPoolClient = new UserPoolClient(this, 'ChatlingsUserPoolClient', {
+      userPool,
+      generateSecret: false,
+    });
+
+
   }
+
+
 }
